592fc1ce416908a7d428cf57340840ade43810a6
Ryan Cox <congmingwudi@gmail.com>
2021-01-06 01:55:25 -0700

    fixed perm set

--- scripts/assign-perm-set-to-user.sh
+++ scripts/assign-perm-set-to-user.sh
@@ -1,4 +1,4 @@
-USER_ALIAS="${USER_ALIAS:-sfdx-platform-event-notifications__qa}"
+USER_ALIAS="${USER_ALIAS:-sfdx-platform-event-notifications__dev}"
 PERM_SET=rc_business_events
 echo assigning permission set $PERM_SET to USER_ALIAS = $USER_ALIAS
 sfdx force:user:permset:assign -n $PERM_SET -u $USER_ALIAS
